1e56f16d6085936676c07d55ad6bf32621772f4c
ishmeetk01 <97965886+ishmeetk01@users.noreply.github.com>
2024-02-23 18:42:04 +0530

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register/user', [AuthController::class, 'register']);
+Route::post('login/user', [AuthController::class, 'login']);
+
+Route::get('jobs/latest',[JobController::class,'index']);
+Route::get('jobs/{id}', [JobController::class, 'show']);
