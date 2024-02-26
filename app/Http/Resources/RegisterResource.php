0b31e4611b6b3f824c9d45a8362249aca8db4255
ANANDKUMAR PATEL <anandpatel95374@gmail.com>
2024-02-26 13:10:10 +0530

    created the login and register resource

---
+++ app/Http/Resources/RegisterResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RegisterResource extends JsonResource
+{
+    public function __construct($resource, $token)
+    {
+        parent::__construct($resource);
+        $this->token = $token;
+    }
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'user' => [
+                'email' => $this->resource->email,
+                'name' => $this->resource->name,
+                'id' => $this->resource->id,
+                'profile_pic' => $this->resource->profile_pic,
+            ],
+            'token' => $this->token,
+        ];
+    }
+}
